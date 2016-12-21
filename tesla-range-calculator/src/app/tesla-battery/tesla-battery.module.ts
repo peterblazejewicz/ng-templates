20a15c26a8514fdb2da7c775d4fe110c6853e16f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-12-21 23:34:59 +0100

    TeslaBattery module

---
+++ tesla-range-calculator/src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,13 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [],
+  exports: []
+})
+export class TeslaBatteryModule { }

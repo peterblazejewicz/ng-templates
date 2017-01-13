bcd26e163fce27103cd816852afeb94ba71d1965
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-01-13 20:36:22 +0100

    Auth0 config based on Env concept

--- auth0/01-login/src/environments/environment.prod.ts
+++ auth0/01-login/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+import { AuthConfig } from '../app/auth/auth-config';
+
 export const environment = {
   production: true
+};
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: '',
+  domain: '',
+  callbackURL: 'http://localhost:4200/'
 };

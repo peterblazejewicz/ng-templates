5268773d1a010bb1aee0eb9c4c35b5ed0f0b9d7e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-01-16 23:36:23 +0100

    Controller code

--- auth0/04-Calling-API/server/src/WebAPIApplication/Controllers/ValuesController.cs
+++ auth0/04-Calling-API/server/src/WebAPIApplication/Controllers/ValuesController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPIApplication.Controllers
@@ -9,36 +6,20 @@ namespace WebAPIApplication.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("ping")]
+        public string Ping()
         {
-            return new string[] { "value1", "value2" };
+            return "All good. You don't need to be authenticated to call this.";
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [Authorize]
+        [HttpGet]
+        [Route("secured/ping")]
+        public string PingSecured()
         {
-            return "value";
+            return "All good. You only get this message if you are authenticated.";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
